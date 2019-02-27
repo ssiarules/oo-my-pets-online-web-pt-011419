f6e696fc54b213f5cd00b6a10dd888008ebe7971
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-27 05:27:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -62,8 +62,7 @@ class Owner
    end
 
    def list_pets
-     "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s),\n
-     and #{pets[:cats].size} cat(s)."
+     "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s),and #{pets[:cats].size} cat(s)."
    end
 
  end
